51d2f4ca6b4b6213d2f6d3ebc9660d6be7e4f0cf
Ashley D <webdevmvp@gmail.com>
2019-12-01 23:14:37 +0000

    Updates

---
+++ precache-manifest.a0a2994a18eca7b524d9c1999b73bb40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4cd07b3b7acc26c2b58c3c5cb8c5fc",
+    "url": "/react-tactoe/index.html"
+  },
+  {
+    "revision": "b93acdbf45a724b98ee2",
+    "url": "/react-tactoe/static/css/main.5e43b05a.chunk.css"
+  },
+  {
+    "revision": "065f1245b0fa761a1136",
+    "url": "/react-tactoe/static/js/2.0e854cdc.chunk.js"
+  },
+  {
+    "revision": "b93acdbf45a724b98ee2",
+    "url": "/react-tactoe/static/js/main.61f5af9e.chunk.js"
+  },
+  {
+    "revision": "de8ea004c9e6f7f90166",
+    "url": "/react-tactoe/static/js/runtime-main.d8ddeead.js"
+  }
+]);
